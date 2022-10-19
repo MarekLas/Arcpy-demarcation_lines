3dc8a4bfd001f3b074fa658f58e0292d21e7dd8d
Marek Laskowski <m.laskowski82@gmail.com>
2022-10-19 13:09:51 +0200

    Second version of the tool

---
+++ linieRozgr_v2.py
@@ -0,0 +1,28 @@
+import arcpy
+import os
+
+aprx = arcpy.mp.ArcGISProject("CURRENT")
+folder = aprx.homeFolder
+arcpy.env.workspace = r"{}\brg_mpzp.gdb".format(folder)
+arcpy.env.overwriteOutput = True
+
+nr_planu = arcpy.GetParameterAsText(0)
+
+mpzpMap = aprx.listMaps(f"{nr_planu}_MPZP")[0]
+
+folder = aprx.homeFolder
+arcpy.env.workspace = r"{}\brg_mpzp.gdb".format(folder)
+arcpy.env.overwriteOutput = True
+
+arcpy.management.Append(r"POWIERZCHNIE\PRZEZNACZENIE\PRZEZ;POWIERZCHNIE\PRZEZNACZENIE\PRZEZ_ULICE;POWIERZCHNIE\PRZEZNACZENIE\PRZEZ_INFR;POWIERZCHNIE\PRZEZNACZENIE\PRZEZ_KOM;POWIERZCHNIE\PRZEZNACZENIE\PRZEZ_MIX", r"{}\brg_mpzp.gdb\BRG_MPZP\LINIE_ROZGR".format(folder), "NO_TEST", 'byt "byt" true true false 255 Text 0 0,First,#', '', '')
+
+linRoz = mpzpMap.listLayers("LINIE_ROZGRANICZAJACE")[0]
+linRozgr = mpzpMap.listLayers("LINIE_ROZGR")[0]
+
+camera = aprx.activeView.camera
+camera.setExtent(arcpy.Describe(linRozgr).extent)
+
+linRoz.visible = False
+
+arcpy.AddMessage(nr_planu)
+arcpy.AddMessage("Brawo! Linie rozgraniczajace utworzone")
